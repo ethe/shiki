e91fa8433691b32d02e7c8cdc6068b41b0846218
Gwo <lambda.guo@gmail.com>
2016-10-14 18:35:44 +0800

    Simplify lexer code.

--- elanus/lexer.py
+++ elanus/lexer.py
@@ -8,12 +8,13 @@ from .token import *
 class Lexer(object):
     def rules(self):
         result = [
-            (r"\n", self.newline),
-            (r"\s+", self.space),
-            (r"\d+\.\d+", self.float),
-            (r"\d+", self.int),
-            (r"\+|-|\*|\/", self.opration),
-            (r"[a-zA-Z_][a-zA-Z_0-9]*(\?|!)?", self.ident)
+            (r"\n",                                       self.newline),
+            (r"\s+",                                      self.space),
+            (r"\d+\.\d+",                                 self.float),
+            (r"\d+",                                      self.int),
+            (r"\+|-|\*|\/|=|==|>|<|\(|\)|\[|\]|\||,",     self.opration),
+            (r"(let|func|do|end|return)\b",               self.keyword),
+            (r"[a-zA-Z_][a-zA-Z_0-9]*(\?|!)?",            self.ident)
         ]
         return result
 
@@ -24,33 +25,10 @@ class Lexer(object):
         if self.remain != '':
             print("Lexer error, can not lex {} at line {}, column {}".format(self.remain, self.line, self.column))
 
-    def newline(self, _, value):
-        result = Token('NEWLINE', value, self.line, self.column)
-        self.line += 1
-        self.column = 1
-        return result
+    macro = ("def {name}(self, _, value):\n"
+             "    result = Token('{type}', value, self.line, self.column)\n"
+             "    self.column += len(value)\n"
+             "    return result")
 
-    def space(self, _, value):
-        result = Token('SPACE', value, self.line, self.column)
-        self.column += len(value)
-        return result
-
-    def float(self, _, value):
-        result = Token('FLOAT', value, self.line, self.column)
-        self.column += len(value)
-        return result
-
-    def int(self, _, value):
-        result = Token('INT', value, self.line, self.column)
-        self.column += len(value)
-        return result
-
-    def opration(self, _, value):
-        result = Token('OPRATION', value, self.line, self.column)
-        self.column += len(value)
-        return result
-
-    def ident(self, _, value):
-        result = Token('IDENT', value, self.line, self.column)
-        self.column += len(value)
-        return result
+    for type in ["NEWLINE", "SPACE", "FLOAT", "INT", "OPRATION", "IDENT", "KEYWORD"]:
+        exec(macro.format(name=type.lower(), type=type))
